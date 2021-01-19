22a65825c28b1db3c1a313b30d7257b78239f3dd
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-01-18 22:28:28 -0300

    initial commit after pre-configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Atlas Gatsby Netlify CMS Starter`,
+    description: `AtlasCodeDev starter template for using Gatsby, NetlifyCMS, Firebase hosting and Github Actions`,
+    author: `@oparin10`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,13 +13,54 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-plugin-material-ui",
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "uploads",
+        path: `${__dirname}/static/img`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "uploads",
+        path: `${__dirname}/src/images`,
+      },
+    },
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {},
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `AtlasCodeDev starter`,
+        short_name: `ACD starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
---
+++ src/templates/Post.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Post({ pageContext }) {
+  return <div>Pass data via gatsby-node.js createPages API via context</div>
+}
+
+export default Post
